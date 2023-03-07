dd61a091779225db80dcbc2d0e525efec0bc9674
VazquezGutierrezMonicaYariana <126636924+VazquezGutierrezMonicaYariana@users.noreply.github.com>
2023-03-06 19:40:06 -0600

    Add files via upload

---
+++ M1/SM3/JAVA/04Arreglos.java
@@ -0,0 +1,15 @@
+// PRACTICA #4
+import java.io.*;
+class Vector 
+  {
+   public static void main(Stirng[] args)
+     {
+      int[] numeros = new int[3];
+      numeros[0] = 5;
+      numeros[1] = 9;
+      numeros[2] = 2;
+      System.out.println("El primer elemento del arreglo es: "+numeros[0];
+      System.out.println("El primer elemento del arreglo es: "+numeros[1];
+      System.out.println("El primer elemento del arreglo es: "+numeros[2];
+     }
+  }
